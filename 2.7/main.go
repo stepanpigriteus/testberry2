21b0c692edb33f572ed0c5020d7cb6e6bc26cd1f
stepanpigriteus <avetor-sport@yandex.ru>
2025-08-18 18:07:01 +0500

    Task 2.7 done

---
+++ 2.7/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func asChan(vs ...int) <-chan int {
+	c := make(chan int)
+	go func() {
+		for _, v := range vs {
+			c <- v
+			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+		}
+		close(c)
+	}()
+	return c
+}
+
+func merge(a, b <-chan int) <-chan int {
+	c := make(chan int)
+	go func() {
+		for {
+			select {
+			case v, ok := <-a: // если канал открыт читаем из а и записываем в переменную v
+				if ok {  // если открыт и v существует - пишем в c
+					c <- v
+				} else {
+					a = nil // если канал закрыт - присваеваем а = nil что навсегда блокирует канал и эта ветка в select больше не сработает
+				}
+			case v, ok := <-b:
+				if ok {
+					c <- v
+				} else {
+					b = nil
+				}
+			}
+			if a == nil && b == nil {
+				close(c) // если оба канала заблокированы - закрываем канал с и выходим
+				return
+			}
+		}
+	}()
+	return c
+}
+
+func main() {
+	rand.Seed(time.Now().Unix())
+	a := asChan(1, 3, 5, 7)
+	b := asChan(2, 4, 6, 8)
+	c := merge(a, b)
+	for v := range c {
+		fmt.Print(v)
+	}
+}
